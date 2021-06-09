608d1a7cc2e80235991dc0b275eb2dd0c46d9e01
cilada bino <fernando.bino.machado@gmail.com>
2021-06-09 19:44:13 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/multiconversor/Sistema.java
@@ -0,0 +1,14 @@
+package com.example.multiconversor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sistema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sistema);
+    }
+}
